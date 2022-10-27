45cb97edf3de684ce67a3818ef44bd38c7793cd2
Benedict Anthony <benedict.ughulu@gmail.com>
2022-10-27 15:09:02 -0700

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,8 +5,8 @@ import profilePic from "../image/IMG-20211218-WA0023.jpg"
 const Profile = () => {
     return (
         <div className="profile">
-            <img src={profilePic} alt="" />
-            <h3>Benedict Ughulu</h3>
+            <img src={profilePic} alt="" id="profile__img" />
+            <h3 id="twitter">Benedict Ughulu</h3>
             {/* slack user name */}
             <h3 style={{ display: "none" }} id="slack">Benedict </h3>
         </div>
