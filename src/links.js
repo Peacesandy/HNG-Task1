0182ef52aafd697b40c826075ab04269ad436f7f
Benedict Anthony <benedict.ughulu@gmail.com>
2022-10-27 15:15:56 -0700

    Update links.js

--- src/links.js
+++ src/links.js
@@ -2,35 +2,41 @@ export const links = [
     {
         id: 1,
         name: "Twitter",
-        url: "https://twitter.com/chrisBenedictA1"
+        url: "https://twitter.com/chrisBenedictA1",
+        linkID:"twiiter"
     },
     {
         id: 2,
         name: "Zuri Team",
-        url: "https://training.zuri.team/"
+        url: "https://training.zuri.team/",
+        linkID:"btn__zuri"
     },
 
     {
         id: 3,
         name: "Zuri Books",
-        url: " http://books.zuri.team"
+        url: " http://books.zuri.team",
+        linkID:"books"
     },
 
     {
         id: 4,
         name: "Python Book",
-        url: "https://books.zuri.team/python-for-beginners?ref_id=benedict"
+        url: "https://books.zuri.team/python-for-beginners?ref_id=benedict",
+        linkID:"book__python"
     },
 
     {
         id: 5,
         name: "background check for coders",
-        url: "https://background.zuri.team"
+        url: "https://background.zuri.team",
+        linkID:"pitch"
     },
     {
         id: 6,
         name: "book design",
-        url: "https://books.zuri.team/design-rules"
+        url: "https://books.zuri.team/design-rules",
+        linkID:"book__design"
     },
 ]
 
