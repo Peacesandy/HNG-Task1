3973184257229a7711ab99893a456c3585ddee61
Benedict Anthony <benedict.ughulu@gmail.com>
2022-10-28 05:43:27 -0700

    updated the profile and link ID

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import vector from "../image/Vector.png";
+// import vector from "../image/Vector.png";
 import I4G from "../image/I4G.png";
 
 const Footer = () => {
     return (
         <div className='container footer'>
-            <img src={vector} alt="" />
+            <h3>Zuri <span>o</span> Intership</h3>
             <h5>HNG Intership 9 frontend task</h5>
             <img src={I4G} alt="" />
         </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import profilePic from "../image/IMG-20211218-WA0023.jpg"
-
+import arrow from "../image/arrow.png"
 
 const Profile = () => {
     return (
         <div className="profile">
             <img src={profilePic} alt="" id="profile__img" />
-            <h3 id="twitter">Benedict Ughulu</h3>
+            <h3>Benedict Ughulu</h3>
             {/* slack user name */}
             <h3 style={{ display: "none" }} id="slack">Benedict </h3>
+            <div className="icon"><img src={arrow} alt="" /></div>
         </div>
     )
 }
