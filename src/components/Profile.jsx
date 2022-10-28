692251f979e36d473a5a81ca1a0cb8261e02f28e
Benedict Anthony <benedict.ughulu@gmail.com>
2022-10-28 06:15:10 -0700

    update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import profilePic from "../image/IMG-20211218-WA0023.jpg"
-import arrow from "../image/arrow.png"
+import arrow from "../image/arrow.png";
+import mobile from "../image/mobile.png";
 
 const Profile = () => {
     return (
@@ -9,7 +10,9 @@ const Profile = () => {
             <h3 id='twitter'>Benedict Ughulu</h3>
             {/* slack user name */}
             <h3 style={{ display: "none" }} id="slack">Benedict </h3>
-            <div className="icon"><img src={arrow} alt="" /></div>
+            <div className="icon desktop"><img src={arrow} alt="" /></div>
+            <div className="icon mobile"><img src={mobile} alt="" /></div>
+
         </div>
     )
 }
