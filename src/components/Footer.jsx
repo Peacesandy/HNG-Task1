9d3f6711d3851e230e1ecc928be750a5763dc494
Benedict Anthony <benedict.ughulu@gmail.com>
2022-10-28 07:23:45 -0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import I4G from "../image/I4G.png";
 const Footer = () => {
     return (
         <div className='container footer'>
-            <h3>Zuri <span>o</span> Intership</h3>
+            <h3>Zuri <span></span> Intership</h3>
             <h5>HNG Intership 9 frontend task</h5>
             <img src={I4G} alt="" />
         </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,18 +2,28 @@ import React from 'react'
 import profilePic from "../image/IMG-20211218-WA0023.jpg"
 import arrow from "../image/arrow.png";
 import mobile from "../image/mobile.png";
+import { useState } from "react"
 
 const Profile = () => {
+    const [show, setShow] = useState(true)
+
     return (
         <div className="profile">
             <img src={profilePic} alt="" id="profile__img" />
             <h3 id='twitter'>Benedict Ughulu</h3>
             {/* slack user name */}
             <h3 style={{ display: "none" }} id="slack">Benedict </h3>
-            <div className="icon desktop"><img src={arrow} alt="" /></div>
-            <div className="icon mobile"><img src={mobile} alt="" /></div>
+            <div className="icon desktop" onClick={() => setShow(!show)} >
+                <img src={arrow} alt="" />
+                <p className={`share ${show && "show"}`}>share</p>
+            </div>
+            <div className="icon mobile" onClick={() => setShow(!show)}>
+                <p className={`share ${show && "show"}`}>share</p>
+                <img src={mobile} alt="" />
+            </div>
 
         </div>
+
     )
 }
 
